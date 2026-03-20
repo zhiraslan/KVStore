76693d754712c768b4874cb98a4443a2959c269e
Zhiraslan Shedzhem <zshedzhem@mail.ru>
2026-03-20 14:49:26 +0300

    draft testing (no GTest)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,34 @@
-int main() {
+#include <iostream>
+#include "Storage.h"
+#include "Service.h"
 
+
+int main() {
+	Storage st{};
+	Service se{ st };
+
+	Status cur = se.set("zhiraslan", "shedzhem");
+	std::cout << static_cast<int>(cur) << std::endl;
+
+	cur = se.set("", "shedzhem");
+	std::cout << static_cast<int>(cur) << std::endl;
+
+	cur = se.set("dina", "shedzhem");
+	std::cout << static_cast<int>(cur) << std::endl;
+
+	auto result = se.get("zhiraslan");
+	if (result.ok()) {
+		std::cout << "Value: " << result.val() << "\n"; // успешный get
+	}
+	else {
+		std::cout << "Get failed: " << static_cast<int>(result.status()) << "\n"; // ошибка
+	}
+
+	result = se.get("");
+	if (result.ok()) {
+		std::cout << "Value: " << result.val() << "\n"; // успешный get
+	}
+	else {
+		std::cout << "Get failed: " << static_cast<int>(result.status()) << "\n"; // ошибка
+	}
 }
